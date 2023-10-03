ea7f3c34edd5c9d326d8102cb740213f892ed41d
elsoapp <115948093+elsoapp@users.noreply.github.com>
2023-10-03 17:47:54 +0300

    Add files via upload

---
+++ scripts/footer.js
@@ -0,0 +1,14 @@
+footer = document.getElementsByClassName("footer-container")[0];
+footer.innerHTML = `
+		<ul>
+			
+			<li><a class="footer-contentLink" href="index.html">Elso</a></li>
+			<li><a class="footer-contentLink" href="privacypolicy.html" target="_blank">Политика конфиденциальности</a></li>
+			<li><a class="footer-contentLink" href="terms.html" target="_blank">Условия использования</a></li>
+			<li><a class="footer-contentLink" href="rules.html" target="_blank">Правила сообщества</a></li>
+			<li><a class="footer-contentLink" href="connection.html" target="_blank">Связаться</a></li>
+			
+		</ul>
+		<br>
+		<p class = "footer-copyright">Copyright © 2023 Elso.</p>
+`;
