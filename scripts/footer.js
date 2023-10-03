fd8119da3b0fc06a2be9e40ad789bdec0c4bf8e9
elsoapp <115948093+elsoapp@users.noreply.github.com>
2023-10-03 18:45:43 +0300

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -6,7 +6,7 @@ footer.innerHTML = `
 			<li><a class="footer-contentLink" href="privacypolicy.html" target="_blank">Политика конфиденциальности</a></li>
 			<li><a class="footer-contentLink" href="terms.html" target="_blank">Условия использования</a></li>
 			<li><a class="footer-contentLink" href="rules.html" target="_blank">Правила сообщества</a></li>
-			<li><a class="footer-contentLink" href="connection.html" target="_blank">Связаться</a></li>
+			<li><a class="footer-contentLink" href="contacts.html" target="_blank">Контакты</a></li>
 			
 		</ul>
 		<br>
